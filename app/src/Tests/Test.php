7f9bd248c33995562ffc6f81a9276e95a78535e5
amir <amir.h.rassafi@gmail.com>
2023-04-16 09:36:32 +0330

    Complete for now

--- app/src/Tests/Test.php
+++ app/src/Tests/Test.php
@@ -1,26 +1,13 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\App;
 
 class Test extends TestCase
 {
+    //we can add out test cases here
     public function testExample()
     {
-        $app = new App();
-        // Register your routes here
-    
-        $request = $this->createMock(Request::class);
-        $response = $this->createMock(Response::class);
-    
-        // Configure the mock request and response as needed
-    
-        $response = $app->handle($request);
-    
-        // Perform assertions on the response object
-        $this->assertSame(200, $response->getStatusCode());
-    }    
+
+    }
     
 }
