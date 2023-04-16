6607c5c41fa874547661a0646e9fed9dc0f34cc9
amir <amir.h.rassafi@gmail.com>
2023-04-16 04:07:56 +0330

    Fix

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 sudo apt update
 sudo apt install -y docker.io docker-compose npm
-docker-compose -f env/docker-compose build --no-cache  
+docker-compose -f env/docker-compose.yml build --no-cache  
 docker-compose -f env/docker-compose.yml up -d
 docker-compose -f env/docker-compose.yml exec worker sqlite3 database/chat.db ".read database/chat.sql"
 docker-compose -f env/docker-compose.yml exec worker chown www-data:www-data -R database/ 
-docker-compose -f env/docker-compose.yml exec worker composer install
 cp readme.md app/readme.md
